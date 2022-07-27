8ac25675f7cee98f2e0eb42099a6999b1bb4b736
Alex Leone <alex@leone.tech>
2022-07-27 15:43:59 -0400

    Published version 4.3.0 and node 16

--- bin/constants.js
+++ bin/constants.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const ORG = process.env.ORG || 'sprouttech';
+const ORG = 'sprouttech';
 const REPO = 'strapi/strapi';
 const BASE_IMAGE_NAME = `${ORG}/base`;
 const STRAPI_IMAGE_NAME = `${ORG}/strapi`;
-const NODE_VERSIONS = [14];
-const LATEST_NODE_VERSION = 14;
+const NODE_VERSIONS = [16];
+const LATEST_NODE_VERSION = 16;
 
 module.exports = {
   ORG,
